9fc3b2f2c6912eb5aa5734384458dd3314539fa1
catapultsoftworks <brich@Brandons-MacBook-Pro-2.local>
2013-12-07 12:24:39 -0500

    basic capistrano hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapTest::Application.config.secret_key_base = 'a7512bbac20c347f1b4936e993b4bdbf1157aa9b6fa65270fc30c94226a02ece887b76fce87d5eeec20df7513f10d0c7e7201ac1451c236689394ec8c8d847aa'
